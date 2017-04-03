9e754d9f5fb665d354e62313f44e80d636d21db1
Fred George <fredgeorge@acm.org>
2017-04-03 13:22:18 +0800

    Fred: Fixed missing require in base rapids_rivers.rb; re-introduced directory for RabbitMQ-specific code

--- lib/rapids_rivers.rb
+++ lib/rapids_rivers.rb
@@ -1,6 +1,5 @@
 require "rapids_rivers/version"
-require "rapids_rivers/packet_problems"
-require "rapids_rivers/river"
+require_relative "rapids_rivers/rabbit_mq/rabbit_mq_rapids"
 
 module RapidsRivers
   # Your code goes here...
